26e7262a967e4470008bcc21c597006ba4c58d04
lasergangers <celloadrian29@protonmail.com>
2022-12-28 06:13:02 +0700

    feat: add time into head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export default function Head() {
-  return (
-    <>
-      <title>My Next.js App</title>
-    </>
-  );
+  const [hydrated, setHydrated] = useState<boolean>(false);
+  const [hour, setHour] = useState<string>("24");
+  const [minute, setMinute] = useState<string>("59");
+  const [second, setSecond] = useState<string>("59");
+
+  const message = `${hour}:${minute}:${second}`;
+
+  useEffect(() => {
+    setHydrated(true);
+
+    const interval = setInterval(() => {
+      const date = new Date();
+
+      let hour = date.getHours().toString().padStart(2, "0");
+      let minute = date.getMinutes().toString().padStart(2, "0");
+      let second = date.getSeconds().toString().padStart(2, "0");
+
+      setHour(hour);
+      setMinute(minute);
+      setSecond(second);
+    }, 1000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  return <>{hydrated && <title>{message}</title>}</>;
 }
